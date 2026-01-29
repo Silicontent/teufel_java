f7f577600d7888a22cac7bf31da50b1ee0984fe9
Luke Johnson <mcmhusker@gmail.com>
2026-01-29 09:53:29 -0600

    Starting on pacification mechanic for CowMixin

--- src/main/java/com/silicontent/teufel/mixin/entity/CowMixin.java
+++ src/main/java/com/silicontent/teufel/mixin/entity/CowMixin.java
@@ -1,5 +1,6 @@
 package com.silicontent.teufel.mixin.entity;
 
+import com.silicontent.teufel.item.ModItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -12,9 +13,17 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Set;
 
 @Mixin(CowEntity.class)
 public abstract class CowMixin extends AnimalEntity {
@@ -65,4 +74,18 @@ public abstract class CowMixin extends AnimalEntity {
 	}
 
 	// TODO: experiment with modifying initGoals() after spawning to allow mob to be "pacified"
+	@Inject(method = "interactMob", at = @At("HEAD"))
+	public void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+		ItemStack itemStack = player.getStackInHand(hand);
+		if (itemStack.isOf(ModItems.PEACE_ESSENCE)) {
+			Set<PrioritizedGoal> goals = this.goalSelector.getGoals();
+			for (PrioritizedGoal g : goals) {
+				System.out.println(g.getGoal().toString());
+			}
+			//this.goalSelector.remove();
+
+			// remove an essence
+			//itemStack.decrement(1);
+		}
+	}
 }
