0bd14d1d29aabe174047d21d5552620650534ea3
Luke Johnson <mcmhusker@gmail.com>
2026-02-09 12:08:52 -0600

    Still not a clue on how to fix this ConcurrentModificationException

---
+++ src/main/java/com/silicontent/teufel/mixin/entity/GoalMixin.java
@@ -0,0 +1,8 @@
+package com.silicontent.teufel.mixin.entity;
+
+import net.minecraft.entity.ai.goal.GoalSelector;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GoalSelector.class)
+public class GoalMixin {
+}
